35e0e679638c4a847fc87225b7c7c0240c07b5fc
bianzheng123 <1317257555@qq.com>
2024-08-12 21:23:47 +0800

    reformat the code

--- src/utils/mips_point.h
+++ src/utils/mips_point.h
@@ -39,215 +39,224 @@
 #include <unistd.h>
 
 
-  float mips_distance(const uint8_t *p, const uint8_t *q, unsigned d) {
+float mips_distance(const uint8_t *p, const uint8_t *q, unsigned d) {
     int result = 0;
     for (int i = 0; i < d; i++) {
-      result += ((int32_t)q[i]) * ((int32_t)p[i]);
+        result += ((int32_t) q[i]) * ((int32_t) p[i]);
     }
-    return -((float)result);
-  }
+    return -((float) result);
+}
 
-  float mips_distance(const int8_t *p, const int8_t *q, unsigned d) {
+float mips_distance(const int8_t *p, const int8_t *q, unsigned d) {
     int result = 0;
     for (int i = 0; i < d; i++) {
-      result += ((int32_t)q[i]) * ((int32_t)p[i]);
+        result += ((int32_t) q[i]) * ((int32_t) p[i]);
     }
-    return -((float)result);
-  }
+    return -((float) result);
+}
 
-  float mips_distance(const float *p, const float *q, unsigned d) {
+float mips_distance(const float *p, const float *q, unsigned d) {
     float result = 0;
     for (int i = 0; i < d; i++) {
-      result += (q[i]) * (p[i]);
+        result += (q[i]) * (p[i]);
     }
     return -result;
-  }
+}
 
 template<typename T>
 struct Mips_Point {
-  using distanceType = float; 
-  //template<typename C, typename range> friend struct Quantized_Mips_Point;
+    using distanceType = float;
+    //template<typename C, typename range> friend struct Quantized_Mips_Point;
 
-  struct parameters {
-    int dims;
-    parameters() : dims(0) {}
-    parameters(int dims) : dims(dims) {}
-  };
+    struct parameters {
+        int dims;
 
-  static distanceType d_min() {return -std::numeric_limits<float>::max();}
-  static bool is_metric() {return false;}
-  T operator [](long i) const {return *(values + i);}
+        parameters() : dims(0) {}
 
-  float distance(const Mips_Point<T>& x) const {
-    return mips_distance(this->values, x.values, params.dims);
-  }
+        parameters(int dims) : dims(dims) {}
+    };
 
-  void prefetch() const {
-    int l = (params.dims * sizeof(T) - 1)/64 + 1;
-    for (int i=0; i < l; i++)
-      __builtin_prefetch((char*) values + i* 64);
-  }
+    static distanceType d_min() { return -std::numeric_limits<float>::max(); }
 
-  long id() const {return id_;}
+    static bool is_metric() { return false; }
 
-  Mips_Point() : values(nullptr), id_(-1), params(0) {}
+    T operator[](long i) const { return *(values + i); }
 
-  Mips_Point(T* values, long id, parameters params)
-    : values(values), id_(id), params(params) {}
-
-  bool operator==(const Mips_Point<T>& q) const {
-    for (int i = 0; i < params.dims; i++) {
-      if (values[i] != q.values[i]) {
-        return false;
-      }
+    float distance(const Mips_Point<T> &x) const {
+        return mips_distance(this->values, x.values, params.dims);
     }
-    return true;
-  }
 
-  bool same_as(const Mips_Point<T>& q) const {
-    return values == q.values;
-  }
+    void prefetch() const {
+        int l = (params.dims * sizeof(T) - 1) / 64 + 1;
+        for (int i = 0; i < l; i++)
+            __builtin_prefetch((char *) values + i * 64);
+    }
 
-  void normalize() {
-    double norm = 0.0;
-    for (int j = 0; j < params.dims; j++)
-      norm += values[j] * values[j];
-    norm = std::sqrt(norm);
-    if (norm == 0) norm = 1.0;
-    for (int j = 0; j < params.dims; j++)
-      values[j] = values[j] / norm;
-  }
+    long id() const { return id_; }
 
-  template <typename Point>
-  static void translate_point(T* values, const Point& p, const parameters& params) {
-    for (int j = 0; j < params.dims; j++) values[j] = (T) p[j];
-  }
+    Mips_Point() : values(nullptr), id_(-1), params(0) {}
 
-  template <typename PR>
-  static parameters generate_parameters(const PR& pr) {
-    return parameters(pr.dimension());}
+    Mips_Point(T *values, long id, parameters params)
+            : values(values), id_(id), params(params) {}
+
+    bool operator==(const Mips_Point<T> &q) const {
+        for (int i = 0; i < params.dims; i++) {
+            if (values[i] != q.values[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool same_as(const Mips_Point<T> &q) const {
+        return values == q.values;
+    }
+
+    void normalize() {
+        double norm = 0.0;
+        for (int j = 0; j < params.dims; j++)
+            norm += values[j] * values[j];
+        norm = std::sqrt(norm);
+        if (norm == 0) norm = 1.0;
+        for (int j = 0; j < params.dims; j++)
+            values[j] = values[j] / norm;
+    }
+
+    template<typename Point>
+    static void translate_point(T *values, const Point &p, const parameters &params) {
+        for (int j = 0; j < params.dims; j++) values[j] = (T) p[j];
+    }
+
+    template<typename PR>
+    static parameters generate_parameters(const PR &pr) {
+        return parameters(pr.dimension());
+    }
 
 private:
-  T* values;
-  long id_;
-  parameters params;
+    T *values;
+    long id_;
+    parameters params;
 };
 
-template<typename T, int range=(1 << sizeof(T)*8) - 1>
-struct Quantized_Mips_Point{
-  using distanceType = float; 
-  
-  struct parameters {
-    float max_val;
-    int dims;
-    parameters(int dims) : max_val(1), dims(dims) {}
-    parameters(float max_val, int dims)
-      : max_val(max_val), dims(dims) {}
-  };
+template<typename T, int range = (1 << sizeof(T) * 8) - 1>
+struct Quantized_Mips_Point {
+    using distanceType = float;
 
-  static distanceType d_min() {return -std::numeric_limits<float>::max();}
-  static bool is_metric() {return false;}
-  
-  //T& operator [] (long j) const {if (j >= d) abort(); return *(values+j);}
-  T operator [] (long j) const {return *(values+j);}
+    struct parameters {
+        float max_val;
+        int dims;
 
-  float distance(int8_t* p, int8_t* q) const {
-    int32_t result = 0;
-    for (int i = 0; i < params.dims; i++){
-      result += (int16_t) p[i] * (int16_t) q[i];
+        parameters(int dims) : max_val(1), dims(dims) {}
+
+        parameters(float max_val, int dims)
+                : max_val(max_val), dims(dims) {}
+    };
+
+    static distanceType d_min() { return -std::numeric_limits<float>::max(); }
+
+    static bool is_metric() { return false; }
+
+    //T& operator [] (long j) const {if (j >= d) abort(); return *(values+j);}
+    T operator[](long j) const { return *(values + j); }
+
+    float distance(int8_t *p, int8_t *q) const {
+        int32_t result = 0;
+        for (int i = 0; i < params.dims; i++) {
+            result += (int16_t) p[i] * (int16_t) q[i];
+        }
+        //return (float) (r * r - result);
+        return (float) -result;
     }
-    //return (float) (r * r - result);
-    return (float) -result;
-  }
 
-  float distance(int16_t* p, int16_t* q) const {
-    int64_t result = 0;
-    for (int i = 0; i < params.dims; i++){
-      result += (int32_t) p[i] * (int32_t) q[i];
+    float distance(int16_t *p, int16_t *q) const {
+        int64_t result = 0;
+        for (int i = 0; i < params.dims; i++) {
+            result += (int32_t) p[i] * (int32_t) q[i];
+        }
+        return (float) -result;
     }
-    return (float) -result;
-  }
 
-  float distance(const Quantized_Mips_Point &x) const {
-    return distance(this->values, x.values);
-  }
-
-  void prefetch() const {
-    int l = (params.dims * sizeof(T) - 1)/64 + 1;
-    for (int i=0; i < l; i++)
-      __builtin_prefetch((char*) values + i * 64);
-  }
-
-  bool same_as(const Quantized_Mips_Point& q){
-    return values == q.values;
-  }
-
-  long id() const {return id_;}
-
-  Quantized_Mips_Point(T* values, long id, parameters p)
-    : values(values), id_(id), params(p)
-  {}
-
-  bool operator==(const Quantized_Mips_Point &q) const {
-    for (int i = 0; i < params.dims; i++) {
-      if (values[i] != q.values[i]) {
-        return false;
-      }
+    float distance(const Quantized_Mips_Point &x) const {
+        return distance(this->values, x.values);
     }
-    return true;
-  }
 
-  void normalize() {
-    std::cout << "can't normalize quantized point" << std::endl;
-    abort();
-  }
+    void prefetch() const {
+        int l = (params.dims * sizeof(T) - 1) / 64 + 1;
+        for (int i = 0; i < l; i++)
+            __builtin_prefetch((char *) values + i * 64);
+    }
 
-  template <typename Point>
-  static void translate_point(T* values, const Point& p, const parameters& params) {
-    for (int j = 0; j < params.dims; j++) {
-      float mv = params.max_val;
-      float pj = p[j];
-      if (pj < -mv || pj > mv) {
-        std::cout << pj << " is out of range, should be in [" << -mv << ":" << mv << "]" << std::endl;
+    bool same_as(const Quantized_Mips_Point &q) {
+        return values == q.values;
+    }
+
+    long id() const { return id_; }
+
+    Quantized_Mips_Point(T *values, long id, parameters p)
+            : values(values), id_(id), params(p) {}
+
+    bool operator==(const Quantized_Mips_Point &q) const {
+        for (int i = 0; i < params.dims; i++) {
+            if (values[i] != q.values[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void normalize() {
+        std::cout << "can't normalize quantized point" << std::endl;
         abort();
-      }
-      int32_t x = std::round(pj * (range/2) / mv);
-      values[j] = (T) x;
     }
-  }
 
-  template <typename PR>
-  static parameters generate_parameters(const PR& pr) {
-    long n = pr.size();
-    int dims = pr.dimension();
-    parlay::sequence<typename PR::T> mins(n);
-    parlay::sequence<typename PR::T> maxs(n);
-    parlay::parallel_for(0, n, [&] (long i) {
-      mins[i] = 0.0;
-      maxs[i] = 0.0;
-      for (int j = 0; j < dims; j++) {
-        mins[i]= std::min(mins[i], pr[i][j]);
-        maxs[i]= std::max(maxs[i], pr[i][j]);}});
-    float min_val = *parlay::min_element(mins);
-    float max_val = *parlay::max_element(maxs);
-    float bound = std::max(max_val, -min_val);
-    // if (sizeof(T) == 1) {
-    //   auto x = parlay::flatten(parlay::tabulate(n, [&] (long i) {
-    //     return parlay::tabulate(dims, [&] (long j) {
-    //       return 128 + (int8_t) (std::round(pr[i][j] * (range/2) / bound));});}));
-    //   auto y = parlay::histogram_by_index(x, 256);
-    //   for (int i = 0; i < 256; i++)
-    //     std::cout << i - 128 << ":" << y[i] << ", ";
-    //   std::cout << std::endl;
-    // }
-    std::cout << "scalar quantization: min value = " << min_val
-              << ", max value = " << max_val << std::endl;
-    return parameters(bound, dims);
-  }
+    template<typename Point>
+    static void translate_point(T *values, const Point &p, const parameters &params) {
+        for (int j = 0; j < params.dims; j++) {
+            float mv = params.max_val;
+            float pj = p[j];
+            if (pj < -mv || pj > mv) {
+                std::cout << pj << " is out of range, should be in [" << -mv << ":" << mv << "]" << std::endl;
+                abort();
+            }
+            int32_t x = std::round(pj * (range / 2) / mv);
+            values[j] = (T) x;
+        }
+    }
+
+    template<typename PR>
+    static parameters generate_parameters(const PR &pr) {
+        long n = pr.size();
+        int dims = pr.dimension();
+        parlay::sequence<typename PR::T> mins(n);
+        parlay::sequence<typename PR::T> maxs(n);
+        parlay::parallel_for(0, n, [&](long i) {
+            mins[i] = 0.0;
+            maxs[i] = 0.0;
+            for (int j = 0; j < dims; j++) {
+                mins[i] = std::min(mins[i], pr[i][j]);
+                maxs[i] = std::max(maxs[i], pr[i][j]);
+            }
+        });
+        float min_val = *parlay::min_element(mins);
+        float max_val = *parlay::max_element(maxs);
+        float bound = std::max(max_val, -min_val);
+        // if (sizeof(T) == 1) {
+        //   auto x = parlay::flatten(parlay::tabulate(n, [&] (long i) {
+        //     return parlay::tabulate(dims, [&] (long j) {
+        //       return 128 + (int8_t) (std::round(pr[i][j] * (range/2) / bound));});}));
+        //   auto y = parlay::histogram_by_index(x, 256);
+        //   for (int i = 0; i < 256; i++)
+        //     std::cout << i - 128 << ":" << y[i] << ", ";
+        //   std::cout << std::endl;
+        // }
+        std::cout << "scalar quantization: min value = " << min_val
+                  << ", max value = " << max_val << std::endl;
+        return parameters(bound, dims);
+    }
 
 private:
-  T* values;
-  long id_;
-  parameters params;
+    T *values;
+    long id_;
+    parameters params;
 };
 
--- src/vamana/neighbors.h
+++ src/vamana/neighbors.h
@@ -41,66 +41,67 @@ void ANN_(Graph<indexType> &G, long k, BuildParams &BP,
           PointRange &Query_Points, QPointRange &Q_Query_Points,
           groundTruth<indexType> GT, char *res_file,
           bool graph_built, PointRange &Points, QPointRange &Q_Points) {
-  parlay::internal::timer t("ANN");
+    parlay::internal::timer t("ANN");
 
-  bool verbose = BP.verbose;
-  using findex = knn_index<QPointRange, indexType>;
-  findex I(BP);
-  indexType start_point;
-  double idx_time;
-  stats<unsigned int> BuildStats(G.size());
-  if(graph_built){
-    idx_time = 0;
-    start_point = 0;
-  } else{
-    I.build_index(G, Q_Points, BuildStats);
-    start_point = I.get_start();
-    idx_time = t.next_time();
-  }
-  std::cout << "start index = " << start_point << std::endl;
-
-  std::string name = "Vamana";
-  std::string params =
-    "R = " + std::to_string(BP.R) + ", L = " + std::to_string(BP.L);
-  auto [avg_deg, max_deg] = graph_stats_(G);
-  auto vv = BuildStats.visited_stats();
-  std::cout << "Average visited: " << vv[0] << ", Tail visited: " << vv[1]
-            << std::endl;
-  Graph_ G_(name, params, G.size(), avg_deg, max_deg, idx_time);
-  G_.print();
-
-  long build_num_distances = parlay::reduce(parlay::map(BuildStats.distances,
-                                                        [] (auto x) {return (long) x;}));
-
-  if(Query_Points.size() != 0) {
-    search_and_parse<Point, PointRange, QPointRange, indexType>(G_, G, Points, Query_Points,
-                                                                Q_Points, Q_Query_Points, GT,
-                                                                res_file, k, false, start_point,
-                                                                verbose);
-  } else if (BP.self) {
-    if (BP.range) {
-      parlay::internal::timer t_range("range search time");
-      double radius = BP.radius;
-      double radius_2 = BP.radius_2;
-      std::cout << "radius = " << radius << " radius_2 = " << radius_2 << std::endl;
-      QueryParams QP;
-      long n = Points.size();
-      parlay::sequence<long> counts(n);
-      parlay::sequence<long> distance_comps(n);
-      parlay::parallel_for(0, G.size(), [&] (long i) {
-        parlay::sequence<indexType> pts;
-        pts.push_back(Points[i].id());
-        auto [r, dc] = range_search(Points[i], G, Points, pts, radius, radius_2, QP, true);
-        counts[i] = r.size();
-        distance_comps[i] = dc;});
-      t_range.total();
-      long range_num_distances = parlay::reduce(distance_comps);
-
-      std::cout << "edges within range: " << parlay::reduce(counts) << std::endl;
-      std::cout << "distance comparisons during build = " << build_num_distances << std::endl;
-      std::cout << "distance comparisons during range = " << range_num_distances << std::endl;
+    bool verbose = BP.verbose;
+    using findex = knn_index<QPointRange, indexType>;
+    findex I(BP);
+    indexType start_point;
+    double idx_time;
+    stats<unsigned int> BuildStats(G.size());
+    if (graph_built) {
+        idx_time = 0;
+        start_point = 0;
+    } else {
+        I.build_index(G, Q_Points, BuildStats);
+        start_point = I.get_start();
+        idx_time = t.next_time();
     }
-  }
+    std::cout << "start index = " << start_point << std::endl;
+
+    std::string name = "Vamana";
+    std::string params =
+            "R = " + std::to_string(BP.R) + ", L = " + std::to_string(BP.L);
+    auto [avg_deg, max_deg] = graph_stats_(G);
+    auto vv = BuildStats.visited_stats();
+    std::cout << "Average visited: " << vv[0] << ", Tail visited: " << vv[1]
+              << std::endl;
+    Graph_ G_(name, params, G.size(), avg_deg, max_deg, idx_time);
+    G_.print();
+
+    long build_num_distances = parlay::reduce(parlay::map(BuildStats.distances,
+                                                          [](auto x) { return (long) x; }));
+
+    if (Query_Points.size() != 0) {
+        search_and_parse<Point, PointRange, QPointRange, indexType>(G_, G, Points, Query_Points,
+                                                                    Q_Points, Q_Query_Points, GT,
+                                                                    res_file, k, false, start_point,
+                                                                    verbose);
+    } else if (BP.self) {
+        if (BP.range) {
+            parlay::internal::timer t_range("range search time");
+            double radius = BP.radius;
+            double radius_2 = BP.radius_2;
+            std::cout << "radius = " << radius << " radius_2 = " << radius_2 << std::endl;
+            QueryParams QP;
+            long n = Points.size();
+            parlay::sequence<long> counts(n);
+            parlay::sequence<long> distance_comps(n);
+            parlay::parallel_for(0, G.size(), [&](long i) {
+                parlay::sequence<indexType> pts;
+                pts.push_back(Points[i].id());
+                auto [r, dc] = range_search(Points[i], G, Points, pts, radius, radius_2, QP, true);
+                counts[i] = r.size();
+                distance_comps[i] = dc;
+            });
+            t_range.total();
+            long range_num_distances = parlay::reduce(distance_comps);
+
+            std::cout << "edges within range: " << parlay::reduce(counts) << std::endl;
+            std::cout << "distance comparisons during build = " << build_num_distances << std::endl;
+            std::cout << "distance comparisons during range = " << range_num_distances << std::endl;
+        }
+    }
 }
 
 template<typename Point, typename PointRange_, typename indexType>
@@ -108,24 +109,27 @@ void ANN(Graph<indexType> &G, long k, BuildParams &BP,
          PointRange_ &Query_Points,
          groundTruth<indexType> GT, char *res_file,
          bool graph_built, PointRange_ &Points) {
-  if (BP.quantize && sizeof(typename PointRange_::T) >= 2) {
-    std::cout << "quantizing build and first pass of search to 1 byte" << std::endl;
-    if (Point::is_metric()) {
-      using QT = uint8_t;
-      using QPoint = Euclidian_Point<QT>;
-      using QPR = PointRange<QT, QPoint>;
-      QPR Q_Points(Points);  // quantized to one byte
-      QPR Q_Query_Points(Query_Points, Q_Points.params);
-      ANN_<Point, PointRange_, QPR, indexType>(G, k, BP, Query_Points, Q_Query_Points, GT, res_file, graph_built, Points, Q_Points);
+    if (BP.quantize && sizeof(typename PointRange_::T) >= 2) {
+        std::cout << "quantizing build and first pass of search to 1 byte" << std::endl;
+        if (Point::is_metric()) {
+            using QT = uint8_t;
+            using QPoint = Euclidian_Point<QT>;
+            using QPR = PointRange<QT, QPoint>;
+            QPR Q_Points(Points);  // quantized to one byte
+            QPR Q_Query_Points(Query_Points, Q_Points.params);
+            ANN_<Point, PointRange_, QPR, indexType>(G, k, BP, Query_Points, Q_Query_Points, GT, res_file, graph_built,
+                                                     Points, Q_Points);
+        } else {
+            using QT = int8_t;
+            using QPoint = Quantized_Mips_Point<QT>;
+            using QPR = PointRange<QT, QPoint>;
+            QPR Q_Points(Points);
+            QPR Q_Query_Points(Query_Points, Q_Points.params);
+            ANN_<Point, PointRange_, QPR, indexType>(G, k, BP, Query_Points, Q_Query_Points, GT, res_file, graph_built,
+                                                     Points, Q_Points);
+        }
     } else {
-      using QT = int8_t;
-      using QPoint = Quantized_Mips_Point<QT>;
-      using QPR = PointRange<QT, QPoint>;
-      QPR Q_Points(Points);
-      QPR Q_Query_Points(Query_Points, Q_Points.params);
-      ANN_<Point, PointRange_, QPR, indexType>(G, k, BP, Query_Points, Q_Query_Points, GT, res_file, graph_built, Points, Q_Points);
+        ANN_<Point, PointRange_, PointRange_, indexType>(G, k, BP, Query_Points, Query_Points, GT, res_file,
+                                                         graph_built, Points, Points);
     }
-  } else {
-    ANN_<Point, PointRange_, PointRange_, indexType>(G, k, BP, Query_Points, Query_Points, GT, res_file, graph_built, Points, Points);
-  }
 }
